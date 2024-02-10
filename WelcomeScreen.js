04cd8736518655295ec34ff737c99da53a49f87a
Marc Langsman <marc@langsman.com>
2024-02-10 15:32:56 +0000

    Added logo to welcome screen

--- WelcomeScreen.js
+++ WelcomeScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View, Text } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 const image = require("./assets/background.jpg");
 
@@ -9,8 +17,14 @@ const WelcomeScreen = () => {
       <ImageBackground
         source={image}
         resizeMode="cover"
-        style={styles.image}
-      ></ImageBackground>
+        style={styles.backgroundImage}
+      >
+        <Image
+          style={styles.logo}
+          resizeMode="contain"
+          source={require("./assets/logo-red.png")}
+        />
+      </ImageBackground>
     </View>
   );
 };
@@ -19,10 +33,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "red",
+    width: "100%",
+    height: "100%",
   },
-  image: {
+  backgroundImage: {
     flex: 1,
     justifyContent: "center",
+    flexDirection: "row",
+  },
+  logo: {
+    height: 692 / 4,
+    width: 692 / 4,
+    top: StatusBar.currentHeight + 150,
   },
 });
 
