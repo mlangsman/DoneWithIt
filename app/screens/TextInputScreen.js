82366c892701d3f49579a7294ef751d557e0ed9b
Marc Langsman <marc@langsman.com>
2024-02-22 15:00:53 +0000

    Added state to text input

--- app/screens/TextInputScreen.js
+++ app/screens/TextInputScreen.js
@@ -1,13 +1,20 @@
 import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from "react";
+import { React, useState } from "react";
 import Screen from "../components/Screen";
 
 import colours from "../config/colours";
 
 const TextInputScreen = () => {
+  const [firstName, setFirstName] = useState("");
+  console.log(firstName);
   return (
     <Screen style={styles.screen}>
-      <TextInput style={styles.textInput} placeholder="First name" />
+      <TextInput
+        style={styles.textInput}
+        placeholder="First name"
+        clearButtonMode="always"
+        onChangeText={(text) => setFirstName(text)}
+      />
     </Screen>
   );
 };
